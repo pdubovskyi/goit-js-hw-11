6b59957582e98c659bba9b93bb0fdce9f47a543f
pdubovskyi <petro.dubovskyi@gmail.com>
2022-12-17 14:18:12 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,80 +1,153 @@
-import './css/styles.css';
-import country from './fetchCountries';
 import Notiflix from 'notiflix';
+import './css/styles.css';
 const axios = require('axios').default;
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// ------------------DOM Elements---------------------
 
 const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
+const inputEl = document.querySelector('input[name="searchQuery"]');
+
+console.log(galleryEl);
 console.log(formEl);
+
+let perPage = 40;
+let page = 0;
+let searchQuery;
+loadBtn.style.display = 'none';
+
+// --------------------------formEl------------------
 
 formEl.addEventListener('submit', onFormEl);
 
 function onFormEl(event) {
   event.preventDefault();
-  const searchQuery = event.currentTarget.searchQuery.value;
+  searchQuery = formEl.elements.searchQuery.value;
+  galleryEl.innerHTML = '';
+  loadBtn.style.display = 'none';
+  page = 1;
+
+  getImage(searchQuery, page, perPage)
+    .then(searchQuery => {
+      console.log(searchQuery);
+      let totalPages = searchQuery.data.totalHits / perPage;
+      // console.log(totalPages);
+      if (searchQuery.data.hits.length > 0) {
+        Notiflix.Notify.success(
+          `Hooray! We found ${searchQuery.totalhits} images.`
+        );
+        renderImages(searchQuery);
+        loadBtn.style.display = 'block';
+
+        if (page < totalPages) {
+          loadBtn.style.display = 'block';
+        } else {
+          loadBtn.style.display = 'none';
+          Notiflix.Notify.info(
+            "We're sorry, but you've reached the end of search results."
+          );
+        }
+      } else {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        galleryEl.innerHTML = '';
+      }
+    })
+    .catch(error => console.log('ERROR: ' + error));
 }
 
-axios
-  .get(
-    'https://pixabay.com/api/?key=32028713-8f4458935a933d773f83236cb&q=cats&image_type=photo'
-  )
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  });
-// const DEBOUNCE_DELAY = 300;
+// ---------------------------HTTP request---------------------
 
-// const inputEl = document.querySelector('#search-box');
-// const countryInfoEl = document.querySelector('.country-info');
-// const listOfCountries = document.querySelector('.country-list');
+function getImage(searchQuery, page, perPage) {
+  return axios
+    .get(
+      `https://pixabay.com/api/?key=32028713-8f4458935a933d773f83236cb&q=${searchQuery}&image_type=photoo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    )
+    .then(responce => {
+      // console.log(responce);
+      return responce;
+    });
+}
 
-// inputEl.addEventListener('input', Lodash(onInputEl, DEBOUNCE_DELAY));
-
-// function onInputEl(e) {
-//   const name = e.target.value.trim();
-//   console.log(name);
-
-//   if (name === '') {
-//     countryInfoEl.innerHTML = '';
-//     listOfCountries.innerHTML = '';
-//   }
-
-//   country(name)
-//     .then(renderCountry)
-//     .catch(error => {
-//       Notiflix.Notify.failure('Oops, there is no country with that name');
-//       listOfCountries.innerHTML = '';
-//       countryInfoEl.innerHTML = '';
-//     });
+// function getImage(searchQuery, page, perPage) {
+//   return axios
+//     .get(
+//       `https://pixabay.com/api/?key=32028713-8f4458935a933d773f83236cb&q=${searchQuery}&image_type=photoo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+//     )
+//     .then(renderImages);
 // }
 
-// function renderCountry(countries) {
-//   console.log(countries.length);
+// --------------------------------Gellery Images------------------
 
-//   if (countries.length > 10) {
-//     return Notiflix.Notify.info(
-//       'Too many matches found. Please enter a more specific name.'
-//     );
-//   } else if (countries.length >= 2 && countries.length <= 10) {
-//     const listMarkup = countries
-//       .map(({ flags, name }) => {
-//         return `<li>
-//         <img src="${flags.svg}" alt="${name.official}" width="30">
-//         <p class="name">${name.official}</p></li>`;
-//       })
-//       .join('');
-//     listOfCountries.innerHTML = listMarkup;
-//     countryInfoEl.innerHTML = '';
-//   } else {
-//     const markup = countries
-//       .map(({ name, capital, population, flags, languages }) => {
-//         return ` <img src="${flags.svg}" alt="${name.official}" width="50">
-//       <h1 class="name">${name.official}</h1>
-//         <p><b>Capital:</b> ${capital}</p>
-//          <p><b>Population:</b> ${population}</p>
-//          <p><b>Languages:</b> ${Object.values(languages)}</p>`;
-//       })
-//       .join('');
-//     countryInfoEl.innerHTML = markup;
-//     listOfCountries.innerHTML = '';
-//   }
-// }
+function renderImages(images) {
+  const arrays = images.data.hits;
+
+  const markup = arrays
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<a href="${largeImageURL} class="gallery-link">
+        <div class="photo-card">
+    <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        <span class="quantity">${likes}</span>
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        <span class="quantity">${views}</span>
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        <span class="quantity">${comments}</span>
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        <span class="quantity">${downloads}</span>
+      </p>
+    </div>
+  </div>
+        </a>`;
+      }
+    )
+    .join('');
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
+
+// -------------------------onLoadBtn-------------------
+
+loadBtn.addEventListener('click', onLoadBtn);
+
+function onLoadBtn(e) {
+  let name = inputEl.value;
+  console.log(name);
+  page += 1;
+
+  getImage(name, page, perPage)
+    .then(name => {
+      let totalPages = name.data.totalHits / perPage;
+      console.log(name.data.hits);
+      renderImages(name);
+      console.log(renderImages(name));
+
+      if (page >= totalPages) {
+        loadBtn.style.display = 'none';
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    })
+    .catch(error => console.log('ERROR: ' + error));
+}
